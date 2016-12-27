b6f3b5a9c1dfdfdc0a6ac5fc029ed2faec3cddb1
Ivan Ivanov <kzv@narod.ru>
2016-12-27 11:35:52 +0000

    beta2

--- modules/main.js
+++ modules/main.js
@@ -47,18 +47,14 @@ var g_currentTime = 0; //Текущее время
 var g_currentSummVTB = 0; //Текущий остаток по кредиту в банке ВТБ
 var g_currentSummSBR = 0; //Текущий остаток по кредиту в банке Сбербанк
 
+var g_fullSummVTB = 0; //Полная сумма выплаченная по кредиту для ВТБ
+var g_fullSummSBR = 0; //Полная сумма выплаченная по кредиту для Сбербанка
+
+var g_bIsEnd = false;
+
 $(function() {
-    //По умолчанию
-    /////////////////////
-    $('#inputSumm').val(1000000); //Начальная сумма кредита
-    $('#inputPeriod').val(24); //Полное время кредита
-    $('#inputProcent').val(10); //Процентная ставка по кредиту
-    $('#inputMonthSumm').val(50000); //Сумма погашения
-    $('#inputSumm').prop('disabled', false);
-    $('#inputPeriod').prop('disabled', false);
-    $('#inputProcent').prop('disabled', false);
-    /////////////////////
-    
+    InitDefaults();
+
     $('#btnContinue').click(function(e) {
         
         const inputSumm = $('#inputSumm').val(); //Начальная сумма кредита
@@ -68,9 +64,10 @@ $(function() {
         $('#inputSumm').prop('disabled', true);
         $('#inputPeriod').prop('disabled', true);
         $('#inputProcent').prop('disabled', true);
+        $('#btnContinue').text('Продолжить');
         
-        if (g_currentSummVTB == 0) g_currentSummVTB = inputSumm;
-        if (g_currentSummSBR == 0) g_currentSummSBR = inputSumm;
+        if (g_currentSummVTB == 0) g_currentSummVTB = parseFloat(inputSumm);
+        if (g_currentSummSBR == 0) g_currentSummSBR = parseFloat(inputSumm);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 //          Расчет для банка ВТБ
@@ -78,8 +75,13 @@ $(function() {
         const kVTB = (inputProcent*Math.pow(1+inputProcent, inputPeriod)) / (Math.pow(1+inputProcent, inputPeriod) - 1);
         const aVTB = kVTB*inputSumm; //Ежемесячный аннуитетный платеж в банке ВТБ
         const deltaVTB = $('#inputMonthSumm').val() - aVTB; //Сумма дополнительного досрочного погашения для ВТБ
+
+        const fProcentVTB = g_currentSummVTB*inputProcent; //Начислено процентов за период в ВТБ
+
+        const fCurrentSummVTB = g_currentSummVTB + fProcentVTB - aVTB - deltaVTB; //Остаток по кредиту в банке ВТБ
         
-        const fCurrentSummVTB = g_currentSummVTB*(1+inputProcent) - aVTB - deltaVTB; //Остаток по кредиту в банке ВТБ
+        g_fullSummVTB += (fCurrentSummVTB < 0) ? (parseFloat($('#inputMonthSumm').val())+fCurrentSummVTB) : parseFloat($('#inputMonthSumm').val()); //Полная сумма погашений на данный момент в ВТБ
+        
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
@@ -90,21 +92,67 @@ $(function() {
         const aSBR = kSBR*g_currentSummSBR; //Ежемесячный аннуитетный платеж в банке Сбербанк
         const deltaSBR = $('#inputMonthSumm').val() - aSBR; //Сумма дополнительного досрочного погашения для Сбербанка
         
-        const fCurrentSummSBR = g_currentSummSBR*(1+inputProcent) - aSBR - deltaSBR; //Остаток по кредиту в банке Сбербанк
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
-
-//        var timeRest = inputPeriod - g_currentTime; //Оставшееся время кредита
+        const fProcentSBR = g_currentSummSBR*inputProcent; //Начислено процентов за период в Сбербанке
         
-        g_currentTime++;
+        const fCurrentSummSBR = g_currentSummSBR + fProcentSBR - aSBR - deltaSBR; //Остаток по кредиту в банке Сбербанк
 
-        $('#table_sber').append('<tr><td>'+g_currentTime+'</td><td>'+aSBR.toFixed(2)+'</td><td>'+fCurrentSummSBR.toFixed(2)+'</td></tr>');
-        $('#table_vtb').append('<tr><td>'+g_currentTime+'</td><td>'+aVTB.toFixed(2)+'</td><td>'+fCurrentSummVTB.toFixed(2)+'</td></tr>');
+        g_fullSummSBR += (fCurrentSummSBR < 0) ? (parseFloat($('#inputMonthSumm').val())+fCurrentSummSBR) : parseFloat($('#inputMonthSumm').val()); //Полная сумма погашений на данный момент в Сбербанке
+
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
+
+        UpdateTables(aSBR, aVTB, fProcentSBR, fProcentVTB, deltaSBR, deltaVTB, fCurrentSummSBR, fCurrentSummVTB);
         
-        g_currentSummVTB = fCurrentSummVTB;
-        g_currentSummSBR = fCurrentSummSBR;
+        if (fCurrentSummSBR < 0 || fCurrentSummVTB < 0)
+            ShowResult();
     });
     
 });
 
+function UpdateTables(aSBR, aVTB, fProcentSBR, fProcentVTB, deltaSBR, deltaVTB, fCurrentSummSBR, fCurrentSummVTB)
+{
+    g_currentTime++;
+    
+    const rowSBR = '<tr id="sbr_'+g_currentTime+'"><td>'+g_currentTime+'</td><td>'+aSBR.toFixed(2)+'</td><td>'+fProcentSBR.toFixed(2)+'</td><td>'+deltaSBR.toFixed(2)+'</td><td>'+fCurrentSummSBR.toFixed(2)+'</td></tr>';
+    const rowVTB = '<tr id="vtb_'+g_currentTime+'"><td>'+g_currentTime+'</td><td>'+aVTB.toFixed(2)+'</td><td>'+fProcentVTB.toFixed(2)+'</td><td>'+deltaVTB.toFixed(2)+'</td><td>'+fCurrentSummVTB.toFixed(2)+'</td></tr>';
+        
+    if (g_currentTime == 1)
+    {
+        $('#table_sber').append(rowSBR);
+        $('#table_vtb').append(rowVTB);
+    }
+    else
+    {
+        $('#sbr_'+(g_currentTime-1)).before(rowSBR);
+        $('#vtb_'+(g_currentTime-1)).before(rowVTB);
+    }
+
+    g_currentSummVTB = fCurrentSummVTB;
+    g_currentSummSBR = fCurrentSummSBR;
+}
+
+function InitDefaults()
+{
+    //Устанавливаем значения по умолчанию
+    /////////////////////
+    $('#inputSumm').val(1000000); //Начальная сумма кредита
+    $('#inputPeriod').val(24); //Полное время кредита
+    $('#inputProcent').val(10); //Процентная ставка по кредиту
+    $('#inputMonthSumm').val(50000); //Сумма погашения
+    $('#inputSumm').prop('disabled', false);
+    $('#inputPeriod').prop('disabled', false);
+    $('#inputProcent').prop('disabled', false);
+    $('#btnContinue').prop('disabled', false);
+    /////////////////////
+}
+
+function ShowResult()
+{
+    $('#header_result').text('Итог');
+    $('#table_result').append('<tr><th>Банк</th><th>Выплачено по кредиту</th></tr>');
+    $('#table_result').append('<tr><td>ВТБ</td><td>'+g_fullSummVTB.toFixed(2)+'</td></tr>');
+    $('#table_result').append('<tr><td>Сбербанк</td><td>'+g_fullSummSBR.toFixed(2)+'</td></tr>');
+    
+    $('#btnContinue').prop('disabled', true);
+}
 
 //browserify --debug ~/workspace/modules/main.js -s htmlEvents > ~/workspace/site/js/main.js
